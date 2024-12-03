afd2751ffbd7eda5f5833d91bfaac7852fb8164c
Kazuyya <zahirraihanhakim.01@gmail.com>
2024-12-03 13:15:56 +0700

    Upload Main Project Data

---
+++ src/assets/assets.js
@@ -0,0 +1,11 @@
+/**
+ * @copyright 2024 codewithsadee
+ * @license Apache-2.0
+ */
+
+import banner from './banner.jpg';
+import iconLogo from './logo-icon.png';
+import logoLight from './logo-light.png';
+import logoDark from './logo-dark.png';
+
+export { banner, iconLogo, logoLight, logoDark };
